4ff70bc047cf3c76f1b278dfcf2a89f2a5e8be0b
Abhay <abhay.android90@gmail.com>
2018-08-20 23:43:14 +0530

    Add files via upload

--- Bike.R
+++ Bike.R
@@ -2,7 +2,7 @@
 rm(list = ls())
 
 # Set Working Directory
-setwd("D:/Bike Renting")
+setwd("D:/bike")
 
 # Get Working Directory
 getwd()
@@ -28,14 +28,6 @@ str(bike_data)
 # Missing Value Analysis
 sum(is.na(bike_data))
 
-# Split 'dteday' column into new Day column
-bike_data <- separate(data = bike_data,col = "dteday",into = c("year","month","day"))
-bike_data$year <- NULL
-bike_data$month <- NULL
-
-## Convert day to int from 
-bike_data$day <- as.numeric(as.character(bike_data$day))
-
 #Multi Histogram for finding out Outlier
 
 multi.hist(bike_data$temp, main = NA, dcol = c("blue", "red"),
@@ -54,7 +46,15 @@ multi.hist(bike_data$cnt, main = NA, dcol = c("blue", "red"),
            dlty = c("solid", "solid"), bcol = "linen")
 
 # We can see some skewness in 'hum'  and  'windspeed'
-y = table(bike_data$yr)
+#
+# # Split 'dteday' column into new Day column
+bike_data <- separate(data = bike_data,col = "dteday",into = c("year","month","day"))
+bike_data$year <- NULL
+bike_data$month <- NULL
+
+## Convert day to int from
+bike_data$day <- as.numeric(as.character(bike_data$day))
+#
 # Plot BoxPlot For them and lets go with Outlier Analysis
 
 numeric_index = sapply(bike_data,is.numeric)
@@ -71,19 +71,22 @@ for (i in 1:length(cnames))
            labs(y=cnames[i],x="cnt")+
            ggtitle(paste("Box plot of cnt for",cnames[i])))
 }
-gridExtra::grid.arrange(gn10,gn11,gn12,ncol=3)
-gridExtra::grid.arrange(gn13,gn14,gn16,ncol=3)
+#gridExtra::grid.arrange(gn10,gn11,gn12,ncol=3)
+#gridExtra::grid.arrange(gn13,gn14,gn16,ncol=3)
+gridExtra::grid.arrange(gn11,gn12,ncol=2)
 
 # hum and windspeed have outliers, Lets remove those values
-
-val_hum = bike_data$hum[bike_data$hum %in% boxplot.stats(bike_data$hum)$out]
-val_windspeed = bike_data$windspeed[bike_data$windspeed %in% boxplot.stats(bike_data$windspeed)$out]
-val_holiday = bike_data$holiday[bike_data$holiday %in% boxplot.stats(bike_data$holiday)$out]
-
-## Remove
-bike_data = bike_data[which(!bike_data$hum %in% val_hum),]
-bike_data = bike_data[which(!bike_data$windspeed %in% val_windspeed),]
-bike_data = bike_data[which(!bike_data$holiday %in% val_holiday),]
+outlier_val <- c('hum' , 'windspeed')
+#
+for(i in outlier_val){
+  val = bike_data[,i][bike_data[,i] %in% boxplot.stats(bike_data[,i])$out]
+  #print(length(val))
+  bike_data[,i][bike_data[,i] %in% val] = NA
+}
+#
+bike_data$hum[is.na(bike_data$hum)] = mean(bike_data$hum, na.rm = T)
+bike_data$windspeed[is.na(bike_data$windspeed)] = mean(bike_data$windspeed, na.rm = T)
+#
 
 ##
 boxplot(bike_data$hum)
@@ -115,12 +118,19 @@ ggplot(bike_data, aes(x = bike_data$hum, y = bike_data$cnt )) + geom_jitter() + 
 
 ggplot(bike_data, aes(x = bike_data$windspeed, y = bike_data$cnt )) + geom_jitter() + geom_smooth(method = loess, formula = y ~ x)
 
-##Lets make towards Model
-# Drop some nouse variable
-df <- bike_data[, -c(1,14:15)]
+# Correlation graph
+library(corrgram)
+corrgram(bike_data[,numeric_index], order = F,
+         upper.panel=panel.pie, text.panel=panel.txt, main = "Correlation Plot")
+vifcor(bike_data[,-16], th = 0.9)
 
-vifcor(df[-13], th = 0.9)
-df$atemp <- NULL # Remove this highly collinear one
+# Lets make towards Model
+# Drop some nouse variable
+#df <- bike_data[, -c(1,11,14:15)]
+df <- bike_data[, -c(1:2,11,14:15)]
+
+vifcor(df[-11], th = 0.9)
+
 vif(df)
 
 # Linear Regression
@@ -130,15 +140,17 @@ summary(lm_model)
 
 # p  0.05 Accept NULL Hypothesis and can say that this variable not relevant to us
 
-predictions_LR = predict(lm_model, df[,1:11])
+predictions_LR = predict(lm_model, df[,1:10])
 library(MLmetrics)
-MAPE(df[,12], predictions_LR)
+MAPE(df[,11], predictions_LR)
+
 # 83%
+
+write.csv(predictions_LR, file = 'D:/bike/countpredictLR.csv')
 # Decision Tree Regression
 
 n = nrow(df)
 trainIndex = sample(1:n, size = round(0.8*n), replace=FALSE)
-#train_index = sample(1:nrow(bike_data), 0.8 = nrow(bike_data))
 train = df[trainIndex ,]
 test = df[-trainIndex ,]
 
@@ -148,20 +160,15 @@ test = df[-trainIndex ,]
 fit = rpart(cnt ~ ., data = train, method = 'anova')
 
 # predictions = predict(fit, test[,-12])
-predictions_LR = predict(fit, test[,-12])
+predictions_TR = predict(fit, test[,-11])
 library(MLmetrics)
 
-mape = function(y, yhat){
-  mean(abs((y - yhat)/y)) * 100
-}
 
 
-mape(test[,12], predictions_LR)
+MAPE(test[,11], predictions_TR)
 # 81%
-#############################################
+write.csv(predictions_TR, file = 'D:/bike/countpredictTR.csv')
+#############################################END##################################################
 
-library(DMwR)
-regr.eval(test[,11], predictions_LR, stats = c('mae', 'mape', 'rmse', 'mse'))
 
-regr.eval(test[,11], predictions_LR, stats = c('mse'))
 
